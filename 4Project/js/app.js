344d0364009aaa03da6f9e8f45771e043af03c6b
Justas <nba.justas@gmail.com>
2023-11-13 16:26:10 +0200

    implemented local storage

--- 4Project/js/app.js
+++ 4Project/js/app.js
@@ -1,16 +1,49 @@
+let tasks = rowGetLocalStorage()
+
+for (let taskId = 0; taskId < tasks.length; taskId++)
+{
+    addTaskToDOM(taskId, tasks[taskId])
+}
+
 createForm = document.querySelector(".createForm")
 addButton = createForm.querySelector("button")
 addButton.addEventListener('click', (event)=>{
     event.preventDefault();
+    
     let rowType = createForm.elements.type.value 
-    let rowSubject = createForm.elements.subject.value 
+    let rowSubjectText = createForm.elements.subject.value 
     let rowPriority = createForm.elements.priority.value
-    let dueDate = createForm.elements.dueDate.value
+    let rowDueDate = createForm.elements.dueDate.value
 
-    let copyRow = rowCreate(rowType, rowSubject, rowPriority, dueDate, 100)
-    let tableBody = document.querySelector('.toDoTable__body')
-    tableBody.appendChild(copyRow)
+    let task = new Object()
+    task.type = rowType
+    task.subject = {
+        text: rowSubjectText,
+        isComplete: false
+    }
+    task.priority = rowPriority
+    task.dueDate = rowDueDate
+    task.percent = 0
+
+    tasks.push(task)
+    taskId = tasks.length-1
+    rowSaveToLocalStorage(tasks)
+    addTaskToDOM(taskId, task)
 })
+
+function rowGetLocalStorage()
+{
+    if(localStorage.getItem('toDoTasks') != null){
+        return JSON.parse(localStorage.getItem('toDoTasks'))
+    }
+    else{
+        return []
+    }
+}
+function rowSaveToLocalStorage(tasks)
+{
+    localStorage.setItem('toDoTasks', JSON.stringify(tasks)) 
+}
 
 function rowSetType(row, iconLink)
 {
@@ -20,18 +53,57 @@ function rowSetType(row, iconLink)
     image.setAttribute("src", iconLink)
 }
 
-function rowSetComplete(row)
+function rowSetTaskComplete(row, boolValue)
 {
     let setClass = "tableRow__subject__content"
     let rowContent = row.querySelector("." + setClass)
-    rowContent.classList.add(setClass + "--completed")
+    let subjectCompleteClass = setClass + "--completed"
+    if(boolValue){
+        rowContent.classList.add(subjectCompleteClass)
+    }
+    else if(element.classList.contains(subjectCompleteClass)){
+        rowContent.classList.remove(subjectCompleteClass)
+    }
 }
-function rowSetSubject(row, text)
+
+function rowSetTaskStatus(row, boolValue)
 {
     let setClass = "tableRow__subject__content"
-    let rowContent = row.querySelector("." + setClass + '> span')
-    rowContent.textContent = text
+    
+    let rowContent = row.querySelector("." + setClass)
+    
+    let subjectCompleteClass = "--completed"
+    
+    if(boolValue){
+        rowContent.classList.add(setClass + subjectCompleteClass)
+    }
+    
+    else if(row.classList.contains(setClass + subjectCompleteClass)){
+        console.log("happens")
+        rowContent.classList.remove(setClass + subjectCompleteClass)
+    }
+
 }
+
+function rowSetSubject(row, subject)
+{
+    console.log(subject)
+    rowSetSubjectText(row, subject.text)
+    rowSetTaskStatus(row, subject.isComplete)
+}
+
+function rowSetSubjectText(row, text)
+{
+    let rowSubjectText = row.querySelector(".tableRow__subject__content > span")
+    rowSubjectText.textContent = text
+}
+
+// function rowSetSubjectComplete(row, boolValue)
+// {
+//     let setClass = "tableRow__subject__content"
+//     let rowContent = row.querySelector("." + setClass + '> span')
+//     rowContent.textContent = text
+// }
 
 function rowSetPriority(row, priorityType)
 {
@@ -102,22 +174,48 @@ function rowCreate(type, subject, priority, dueDate, percent)
     else if(type == 'diamond'){
         rowSetType(newRow, "resources/diamond.svg")
     }
-
+    // rowSetSubjectText(newRow, subject.text)
     rowSetSubject(newRow, subject)
     rowSetPriority(newRow,priority)
     rowSetDueDate(newRow, dueDate)
     rowSetStatusBar(newRow, percent)
     rowSetNewModifiedDate(newRow)
-    
-    rowDeleteButton = newRow.querySelector('button')
-    rowDeleteButton.addEventListener('click', ()=>{
-        newRow.remove()
-    })
-
-    rowSubjectContent = newRow.querySelector(".tableRow__subject__content")
-    rowSubjectContent.addEventListener('click', ()=>{
-        rowSetComplete(newRow)
-    })
 
     return newRow
+}
+
+function addTaskToDOM(taskId, task)
+{
+    let row = rowCreate(task.type, task.subject, task.priority, task.dueDate, task.percent)
+    rowAddClickEvents(taskId, row)
+
+    let tableBody = document.querySelector('.toDoTable__body')
+    tableBody.appendChild(row)
+}
+
+function rowAddClickEvents(taskId, row)
+{
+    //type button 
+    rowSubjectContent = row.querySelector(".tableRow__type__content")
+    rowSubjectContent.addEventListener('click', ()=>{
+        // console.log(tasks)
+        if(tasks[taskId].subject.isComplete == false){
+            tasks[taskId].subject.isComplete = true
+        }
+        else{
+            console.log("test")
+            tasks[taskId].subject.isComplete = false
+        }
+        // console.log(tasks)
+        rowSaveToLocalStorage(tasks)
+        rowSetSubject(row, tasks[taskId].subject)
+    })
+
+    //delete button
+    rowDeleteButton = row.querySelector('.tableRow__delete__content')
+    rowDeleteButton.addEventListener('click', ()=>{
+        tasks.splice(taskId, 1);
+        rowSaveToLocalStorage(tasks)
+        row.remove()
+    })
 }

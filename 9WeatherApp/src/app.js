99d6d9fed75f3ac92c4b359f3b6397ada1d41986
gedasma <gedasma@gmail.com>
2023-12-02 19:32:11 +0200

    style and visual base

--- 9WeatherApp/src/app.js
+++ 9WeatherApp/src/app.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import searchCode from "./modules/searchCode";
 
 searchCode();
--- 9WeatherApp/webpack.mix.js
+++ 9WeatherApp/webpack.mix.js
@@ -2,4 +2,5 @@ let mix = require('laravel-mix')
 
 mix.js('src/app.js', 'public')
 .sass('src/scss/style.scss', 'css')
+.copy('src/assets', 'public/assets')
 .setPublicPath('public')
